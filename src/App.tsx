e04f8a8c62a3ae11e1d0d9db86dff8a5bcdba4ab
ZlDE0N <Ramon.Pereirae@gmail.com>
2025-03-17 16:16:50 +0000

    Feat: New structure for login, module auth, create components for register, forgort password, and relational views.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Home from "./components/home";
 import routes from "tempo-routes";
 import LandingPage from "./components/landing/LandingPage";
 import OnboardingRoutes from "./components/onboarding/OnboardingRoutes";
+import LoginPage from "./components/auth/LoginPage";
+import RegisterPage from "./components/auth/RegisterPage";
+import ForgotPasswordPage from "./components/auth/ForgotPasswordPage";
 
 function App() {
   const tempoRoutes =
@@ -24,6 +27,10 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/onboarding/*" element={<OnboardingRoutes />} />
           <Route path="/dashboard" element={<Home />} />
+          <Route path="/signin" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          {import.meta.env.VITE_TEMPO && <Route path="/tempobook/*" />}
         </Routes>
         {tempoRoutes}
       </>
